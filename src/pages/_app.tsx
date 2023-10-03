e6af301ae6a2d9172aaf0b1452bdf07e6ac08e2f
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2023-10-03 14:26:20 +0200

    Reproduction of issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { pathname } = useRouter();
+
+  const isHomePage = pathname === "/";
+  return (
+    <MantineProvider
+      forceColorScheme={isHomePage ? "dark" : undefined}
+      defaultColorScheme="auto"
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
---
+++ src/pages/second.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@mantine/core";
+
+export default function SecondPage() {
+  return (
+    <Box>
+      <Text>This is the second page</Text>
+    </Box>
+  );
+}
